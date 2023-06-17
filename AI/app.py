728a3663929047f6ecbb974359e1f92856a66204
anslava00 <anslava00@mail.ru>
2023-06-17 15:32:01 +1000

    change model

--- AI/app.py
+++ AI/app.py
@@ -1,58 +1,64 @@
 from flask import Flask, request, jsonify
 import numpy as np
 from flask_cors import CORS
-from torchvision import transforms, models
+# from torchvision import transforms, models
+# import torch
 from PIL import Image
-import torch
 import os.path
 import json
 import io
-
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from skimage import transform
+import tensorflow.keras.utils as image
+from tensorflow.keras.preprocessing.image import img_to_array
+from sklearn.cluster import KMeans
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/image/upload', methods = ['POST'])
+def image_to_pandas(image):
+    df = pd.DataFrame([image[:,:,0].flatten(),
+                       image[:,:,1].flatten(),
+                       image[:,:,2].flatten()]).T
+    df.columns = ['Red_Channel','Green_Channel','Blue_Channel']
+    return df
+
+@app.route('/image/upload', methods=['POST'])
 def hello():
     root_img = request.files['file'].read()
-    root_par_model = 'res18_fisrt.pth'
+    root_par_model = 'model.h5'
+    model = load_model(root_par_model)
 
-    model = models.resnet18(pretrained=True)
-    model.load_state_dict(torch.load(root_par_model))
+    val_datagen = ImageDataGenerator(rescale=1. / 255.)
+    img = Image.open(io.BytesIO(root_img)).convert('RGB')
+    X = np.array([img_to_array(img)])
+    pred_proba = model.predict(val_datagen.flow(X))[0][0]
+    pred = 0
+    if pred_proba >= 0.5:
+        pred = 1
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
+    image_list = []
+    df_rgb_img = image_to_pandas(X)
+    kmeans = KMeans(n_clusters=3, random_state=42).fit(df_rgb_img)
+    result = kmeans.labels_.reshape(X.shape[0], X.shape[1])
+    for n, ax in enumerate(3):
+        imge = img
+        img_array = np.array(imge)
+        img_array[:, :, 0] = img_array[:, :, 0] * (result == [n])
+        img_array[:, :, 1] = img_array[:, :, 1] * (result == [n])
+        img_array[:, :, 2] = img_array[:, :, 2] * (result == [n])
+        image_list.append(Image.fromarray(img_array))
 
-    loss = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3 )
+    #image_list размер 3 картинки
+    # image_list[0]
+    # image_list[1]
+    # image_list[2]
+    return json.dumps(pred)
 
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
-
-    transform = transforms.Compose([
-        transforms.CenterCrop(2300),
-        transforms.Resize((224,224)),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
-
-    image_tensor = transform(Image.open(io.BytesIO(root_img)).convert('RGB')).unsqueeze(0)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    image_tensor = image_tensor.to(device)
-    model = model.to(device)
-
-    model.eval()
-
-    with torch.no_grad():
-        output = model(image_tensor)
-
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    predicted_class = torch.argmax(probabilities).item()
-
-    return json.dumps(predicted_class)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
